829f6fd300c6bb80256426fad0577ec4739d2a93
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-02-28 16:47:52 +0200

    Separate test and compile scopes.

--- bulky_sources_plugin/src/main/scala/example/BulkySourcesPlugin.scala
+++ bulky_sources_plugin/src/main/scala/example/BulkySourcesPlugin.scala
@@ -4,14 +4,20 @@ import sbt.Keys._
 import sbt._
 
 object BulkySourcesPlugin extends AutoPlugin {
-  // Declare a task
-  // taskKey -- method is the description of task that will appear when do 'tasks -V'
-  lazy val bulkySources = taskKey[Seq[(Int, File)]](
-    "Return Seq of files that line of code more then 'bulkyThresholdInLines'"
-  )
-  lazy val bulkyThresholdInLines = settingKey[Int](
-    "Min bulky threshold lines, files that have more lines is bulky. Default is 100."
-  )
+
+  // by defining autoImport, the settings are automatically imported into user's `*.sbt`
+  object autoImport {
+    // Declare a task
+    // taskKey -- method is the description of task that will appear when do 'tasks -V'
+    lazy val bulkySources = taskKey[Seq[(Int, File)]](
+      "Return Seq of files that line of code more then 'bulkyThresholdInLines'"
+    )
+    lazy val bulkyThresholdInLines = settingKey[Int](
+      "Min bulky threshold lines, files that have more lines is bulky. Default is 100."
+    )
+  }
+
+  import autoImport._
 
   // Then define the task as part of a sequence of settings, called projectSettings
   // This method is called by sbt to find the list of settings
@@ -20,13 +26,29 @@ object BulkySourcesPlugin extends AutoPlugin {
     bulkyThresholdInLines := 100,
 
     bulkySources := {
-      val compileFiles = (sources in Compile).value
-      val testCompileFiles = (sources in Test).value
-      val result = compileFiles ++ testCompileFiles
-      result.map { file =>
+      val files = (sources in Compile).value
+      files.map { file =>
         val linesCount = sbt.IO.readLines(file).filterNot(_.isBlank).size
         (linesCount, file)
-      }.filter { case (linesCount, _) => linesCount >= bulkyThresholdInLines.value }
+      }.filter { case (numberOfLines, _) => numberOfLines >= bulkyThresholdInLines.value }
+        .sortWith { case (first, second) =>
+          val (firstNumberOfLines, _) = first
+          val (secondNumberOfLines, _) = second
+          firstNumberOfLines > secondNumberOfLines
+        }
+    },
+
+    bulkySources in Test := {
+      val files = (sources in Test).value
+      files.map { file =>
+        val linesCount = sbt.IO.readLines(file).filterNot(_.isBlank).size
+        (linesCount, file)
+      }.filter { case (numberOfLines, _) => numberOfLines >= bulkyThresholdInLines.value }
+        .sortWith { case (first, second) =>
+          val (firstNumberOfLines, _) = first
+          val (secondNumberOfLines, _) = second
+          firstNumberOfLines > secondNumberOfLines
+        }
     }
   )
 }
